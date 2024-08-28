f8d1e5ad2dd0d6969c3fb8388983e58b492cdd86
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-08-28 10:37:15 +0530

    Added comments in K3.java, K4.java

--- K3.java
+++ K3.java
@@ -6,3 +6,6 @@ public class K3{
 	System.out.println(sum1);
     }
 }
+
+// The name of the file should be same as the name of the public class. So, this file should be K3.java
+// Every Java program requires a main method.  This main method is written inside the public class.  main() method is public because it needs to be accessed from anywhere. main() method is static because the JVM can call it without having to create an instance. main() method is void because it does not return anything. main() method takes a string array as a parameter. ie. it can take 1 string, 2 strings, 3 strings...or any number of strings.  In this example, we are providing 2 string parameters
--- K4.java
+++ K4.java
@@ -5,3 +5,7 @@ public class K4{
 	System.out.println(square1);
     }
 }
+
+//Instead of String[] args,  we have used String[] params. We can use any name, but args is the normal convention
+// javac K4.java - If this executes correctly, then K4.class will be created
+// java K4.java 5 - Here we are passing one argument. Whatever is passed is considered as a String. To convert to an Integer, we use Integer.parseInt().  Here Integer is the class, parseInt() is the method
