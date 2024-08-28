d68db98dec7f0c6371bf9322ade180cf746ee346
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-08-28 13:28:52 +0530

    Mathematical tables, for loop, nested for loop

---
+++ K7.java
@@ -0,0 +1,13 @@
+public class K7{
+    public static void main(String[] args){
+	System.out.println("3" + "*" + "1" + "=" + "3");
+	System.out.println("3" + "*" + "2" + "=" + "6");
+	System.out.println("3" + "*" + "3" + "=" + "9");
+   }
+}
+
+
+
+//javac -d bin K7.java
+//java -cp bin K7
+
