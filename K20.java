78f157f4fe5df0758d6ef39964415e85febd1ead
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-08-29 14:34:23 +0530

    char value can be incremented
    char can be assigned to an integer

---
+++ K20.java
@@ -0,0 +1,21 @@
+public class K20{
+    public static void main(String[] args){
+	char ch1, ch2;
+	ch1='A';
+	System.out.println(ch1);
+	ch1++;
+	System.out.println(ch1);
+
+	int n1;
+	n1=ch1;
+	System.out.println(n1);
+    }
+}
+//javac -d bin K20.java
+//java -cp bin K20
+//Although char is designed to hold Unicode characters, it can also be used as an integer type
+//Arithmetic operations can be performed on char type. - Eg Add 2 characters
+
+
+
+
