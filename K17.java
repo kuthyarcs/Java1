71945773c6d4c8e1fdb2cb5776057256809dbb6a
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-08-29 14:06:46 +0530

    ASCII values for upper, lower English alphabets
    uppercase - starts from 65,   lowercase starts from 97,   (char)(65) -- A,      (char)(97) -- a

---
+++ K17.java
@@ -0,0 +1,17 @@
+public class K17{
+    public static void main(String[] args){
+	int upper1=65;
+	int size=26;
+	for(int i=upper1;i<upper1+size;i++){
+	    System.out.println((char)i);
+	}
+    }
+}
+//javac -d bin K17.java
+//java -cp bin K17
+//ASCII value of A to Z is  65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89
+//char(65) - A
+//char(66) - B
+
+
+
