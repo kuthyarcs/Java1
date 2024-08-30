f6b116d2f7b7eeba481779970de0e18f75e3acc2
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-08-30 11:02:08 +0530

    Create array with 12 integers for days in each month
    Array initializer - list of comma-separated expressions surrounded by curly braces

---
+++ K27.java
@@ -0,0 +1,13 @@
+public class K27{
+    public static void main(String[] args){
+	int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
+	System.out.println("April has "+days[3]+" days");
+    }
+}
+//javac -d bin K27.java
+//java -cp bin K27
+//Java-HerbertSchildt-Chap3
+
+
+
+
