bc2c866d215cf3aa0e2f9fb04b03d013a755a104
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-02 12:13:44 +0530

    Ternary Operator

---
+++ K43.java
@@ -0,0 +1,17 @@
+public class K43{
+    public static void main(String[] args){
+	String status="";
+	int marks=71;
+	status=marks>=70?"Selected":"Not Selected";
+	System.out.println(status);
+	
+    }
+}
+//javac -d bin K43.java
+//java -cp bin K43
+// Ternary Operator
+//Java-HerbertSchildt-Chap3
+
+
+
+
