576bc6e91839333ea368c6f335725ac867951008
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-02 10:37:13 +0530

    After 4 left shifts, the value overflows.
    1 bit is shifted to bit 31. So, number is negative.

---
+++ K39.java
@@ -0,0 +1,16 @@
+public class K39{
+    public static void main(String[] args){
+	int num1=0xFFFFFFE;
+	for (int i=0;i<4;i++){
+	    num1=num1<<1;
+	    System.out.println(num1);}
+    }
+}
+//javac -d bin K39.java
+//java -cp bin K39
+// 
+//Java-HerbertSchildt-Chap3
+
+
+
+
