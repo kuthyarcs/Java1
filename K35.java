5c64b91c4f2a79c8f319e2f012c2afd70fe6a5aa
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-02 09:28:29 +0530

    Increment and Decrement

---
+++ K35.java
@@ -0,0 +1,22 @@
+public class K35{
+    public static void main(String[] args){
+	int a=1;
+	int b=2;
+	int c=3;
+	a +=5;
+	b*=4;
+	c+=a*b;
+	c%=6;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+    }
+}
+//javac -d bin K35.java
+//java -cp bin K35
+// Arithmetic Compound Assignment Operators
+//Java-HerbertSchildt-Chap3
+
+
+
+
