48bae08dce1187e923b8b3b954e0c7b176783854
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-02 16:07:17 +0530

    While loop

---
+++ K47.java
@@ -0,0 +1,16 @@
+public class K47{
+    public static void main(String[] args){
+	int n=10;
+	while(n>0){
+	    System.out.println("Line: "+n);
+	    n--;}
+    }
+}
+//javac -d bin K47.java
+//java -cp bin K47
+// While loop
+//Java-HerbertSchildt-Chap4
+
+
+
+
