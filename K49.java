1755aefebe1b0ae0770c6daaabcdc383df2cfa34
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-02 16:24:14 +0530

    Do while loop 2 different ways

---
+++ K49.java
@@ -0,0 +1,22 @@
+public class K49{
+    public static void main(String[] args){
+	int n1=5;
+	int n2=3;
+	do {
+	    System.out.println("Line: "+n1);
+	    n1--;}
+	    while(n1>0);
+
+	do {
+	    System.out.println("New Line: "+n2);}
+	    while(--n2>0);
+    }
+}
+//javac -d bin K49.java
+//java -cp bin K49
+// While loop - 2 different ways
+//Java-HerbertSchildt-Chap4
+
+
+
+
