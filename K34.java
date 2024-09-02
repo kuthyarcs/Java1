a644e6741d79c60433e3d0e70e4ec8afbaa0e027
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-02 09:27:59 +0530

    Arithmetic Compound Assignment Operator

---
+++ K34.java
@@ -0,0 +1,16 @@
+public class K34{
+    public static void main(String[] args){
+	int x=42;
+	double y=42.5;
+	System.out.println(x%10);
+	System.out.println(y%10);
+    }
+}
+//javac -d bin K34.java
+//java -cp bin K34
+// 
+//Java-HerbertSchildt-Chap3
+
+
+
+
