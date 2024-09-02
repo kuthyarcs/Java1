ad7fee9615af9cdcc09bd5c7987b7bfb91c22a70
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-02 09:04:52 +0530

    Basic Arithmetic Operators

---
+++ K33.java
@@ -0,0 +1,33 @@
+public class K33{
+    public static void main(String[] args){
+	int a=1+1;   //integers
+	int b=a+3;
+	int c=b/4;
+	int d=c-a;
+	int e=-d;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println();
+	double da=1+1;  //double
+	double db=da*3;
+	double dc=db/4;
+	double dd=dc-a;
+	double de=-dd;
+	System.out.println(da);
+	System.out.println(db);
+	System.out.println(dc);
+	System.out.println(dd);
+	System.out.println(de);
+    }
+}
+//javac -d bin K33.java
+//java -cp bin K33
+// Basic Arithmetic Operator
+//Java-HerbertSchildt-Chap3
+
+
+
+
