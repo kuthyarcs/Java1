6ae21c482dd902d6e28a7eca89648580fc43077e
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-03 11:33:31 +0530

    Using break to exit a For Loop

---
+++ K58.java
@@ -0,0 +1,17 @@
+public class K58{
+    public static void main(String[] args){
+	for (int i=0;i<100;i++){
+	    if(i==10){
+		break;}
+	    System.out.println(i);}
+	System.out.println("Loop complete");
+    }
+}
+//javac -d bin K58.java
+//java -cp bin K58
+// 
+//Java-HerbertSchildt-Chap4
+
+
+
+
