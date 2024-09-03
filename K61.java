cb097c26af05340fc83530eb7ff42390afae5150
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-03 11:39:46 +0530

    Continue statement

---
+++ K61.java
@@ -0,0 +1,17 @@
+public class K61{
+    public static void main(String[] args){
+	for(int i=0;i<10;i++){
+	    System.out.print(i + " ");
+	    if(i%2==0){
+		continue;}
+	    System.out.println();}
+    }
+}
+//javac -d bin K61.java
+//java -cp bin K61
+// 
+//Java-HerbertSchildt-Chap4
+
+
+
+
