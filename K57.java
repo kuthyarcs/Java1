cec95e00831daf81539fb78ff446bfde3d87da27
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-03 11:33:04 +0530

    Nested For Loop

---
+++ K57.java
@@ -0,0 +1,16 @@
+public class K57{
+    public static void main(String[] args){
+	for (int i=0;i<10;i++){
+	    for(int j=0;j<10;j++)
+		System.out.print("$");
+	    System.out.println();}
+    }
+}
+//javac -d bin K57.java
+//java -cp bin K57
+// 
+//Java-HerbertSchildt-Chap4
+
+
+
+
