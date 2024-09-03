34d19cd7db065dfd7fbf7d88268e6ac6c9196b3e
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-09-03 11:34:10 +0530

    While loop break statement

---
+++ K59.java
@@ -0,0 +1,19 @@
+public class K59{
+    public static void main(String[] args){
+	int i=0;
+	while (i<100){
+	    if(i==10)
+		break;
+	    System.out.println(i);
+	    i++;}
+	System.out.println("Loop complete");	
+    }
+}
+//javac -d bin K59.java
+//java -cp bin K59
+// 
+//Java-HerbertSchildt-Chap4
+
+
+
+
