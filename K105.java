eb23d8cbafbadb3ecab0675b47b9170e87711fae
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-10-15 12:54:41 +0530

    Primitive int max value

---
+++ K105.java
@@ -0,0 +1,22 @@
+public class K105 {
+    public static void main(String[] args){
+        int n1=10;
+        int n2=100;
+        int n3=1000;
+        int n4=10000;
+        int n5=100000;
+        int n6=1000000;
+        int n7=10000000;
+        int n8=100000000;
+        int n9=1000000000;
+        //int n10=10000000000; shows compilation error
+        
+        int n10=2140000000;
+        int n11=2147000000;
+        int n12=2147480000;
+        int n13=2147483000;
+        int n14=2147483600;
+        int n15=2147483640;
+        //int n16=2147483650;  shows compilation error
+    }
+}
