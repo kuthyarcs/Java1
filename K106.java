2ff41ffbdd7b5cec5cbe475ad6234f20d0bf365e
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-10-15 13:03:54 +0530

    Primitives display Max and Min values

---
+++ K106.java
@@ -0,0 +1,29 @@
+public class K106 {
+    public static void main(String[] args){
+        System.out.println("Primitives Max and Min values");
+        byte b1=0;
+        short s1=0;
+        int n1=0;
+        long l1=0;
+        
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println();
+        
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Double.MIN_VALUE);
+    }
+    
+}
