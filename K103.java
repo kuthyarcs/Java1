59b91555885c428460c508ca8558617ef3ccb853
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-10-15 12:41:19 +0530

    primitive byte max and min values

--- K103.java
+++ K103.java
@@ -2,9 +2,15 @@ public class K103 {
     public static void main(String[] args) {
         System.out.println("Default values of Primitives");
         byte b1=10;
-        for(int i=1;i<10;i++){
-            System.out.println(b1*8*i);
-        }
+        byte b2=100;
+        byte b3=120;
+        byte b4=127;
+        //byte b5=128; //Compiler shows error
+        
+        byte b6 = -10;
+        byte b7 = -100;
+        byte b8 = -127;
+        byte b9 = -128;
+        //byte b10= -129; //Compiler shows error       
     }
-    
 }
