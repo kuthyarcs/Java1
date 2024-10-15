acf6d36edda82db032796ca32a066ad8f6178d2a
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-10-15 12:39:47 +0530

    Primitive byte. If max value exceeds shows error

---
+++ K103.java
@@ -0,0 +1,10 @@
+public class K103 {
+    public static void main(String[] args) {
+        System.out.println("Default values of Primitives");
+        byte b1=10;
+        for(int i=1;i<10;i++){
+            System.out.println(b1*8*i);
+        }
+    }
+    
+}
