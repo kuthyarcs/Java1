d776710ba13d5d7c6680241981c5ca26012adce5
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-10-15 13:18:30 +0530

    Print USN of students in each section
    Need to have 3 digits for the number

---
+++ K107.java
@@ -0,0 +1,11 @@
+public class K107 {
+    public static void main(String[] args){
+        String s1="4MW23CS";
+        String usn=s1;
+        for(int i=1;i<66;i++){
+            usn=s1+String.valueOf(i);
+            System.out.println(usn);
+        }
+    }
+    
+}
