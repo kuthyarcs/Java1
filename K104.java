36be196812402deda1bb9a5c1d418d3b89985d17
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-10-15 12:47:40 +0530

    Primitive short max and min values

---
+++ K104.java
@@ -0,0 +1,18 @@
+public class K104 {
+    public static void main(String[] args){
+        short s1=10;
+        short s2=100;
+        short s3=1000;
+        short s4=10000;
+        short s5=25000;
+        short s6=31000;
+        short s7=32000;
+        short s8=32767;
+        //short s9=32768;   Shows error
+        
+        short s9= -32000;
+        short s10= -32767;
+        short s11= -32768;
+        //short s12= -32769; shows error
+    }
+}
