614c5820a549950d55699e01788597ad7800c979
kuthyarcs <kuthyar.cs@sode-edu.in>
2024-12-14 20:52:02 +0530

    Passing arguments in command line args[0] args[1]

---
+++ K166.java
@@ -0,0 +1,11 @@
+package pkg2;
+
+public class K166 {
+
+	public static void main(String[] args) {
+		int num1=Integer.parseInt(args[0]);
+		int num2=Integer.parseInt(args[1]);
+		int sum1=num1+num2;
+		System.out.println(sum1);
+	}
+}
---
+++ K167.java
@@ -0,0 +1,11 @@
+package pkg2;
+import java.util.Scanner;
+public class K167 {
+
+	public static void main(String[] args) {
+		Scanner sc1=new Scanner(System.in);
+		int num1=sc1.nextInt();
+		int num2=sc1.nextInt();
+		int sum1=num1+num2;
+		System.out.println(sum1);	}
+}
